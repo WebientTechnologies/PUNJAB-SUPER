2340d95fa01b252f40aa87f45879c2ff38b3aa1d
IshfaqKhawaja <ishfaqkhawaja312@gmail.com>
2023-06-13 20:52:59 +0530

    Jun 13

--- lib/screen/lucky100PS/lucy_100_PS.dart
+++ lib/screen/lucky100PS/lucy_100_PS.dart
@@ -29,11 +29,18 @@ class _Lucky100PSState extends State<Lucky100PS> {
     if (number == -1) {
       return const SizedBox.shrink();
     }
+
+    var rowConstant = height * 0.08;
+    var columnConstant = width * 0.025;
+    // print('row is $row and column is $column and row Conts is $rowConstant');
+
     return Positioned(
-      left: column * 2 * width,
-      top: row * 8 * height,
+      left: column * columnConstant + column * 4,
+      top: row < 4
+          ? row * rowConstant - row * 2 - row
+          : row * rowConstant - row * 3.5 - row,
       child: CircleAvatar(
-        radius: ratio * 13,
+        radius: ratio * 15,
       ),
     );
   }
@@ -62,212 +69,208 @@ class _Lucky100PSState extends State<Lucky100PS> {
 
             percentX = x / width;
             percentY = y / height;
-            // print('$percentX, $percentY');
           },
-          child: Stack(
-            children: [
-              SizedBox(
-                child: Image.asset(
-                  'assets/img/SecondMatka.png',
-                  height: double.maxFinite,
-                  width: double.maxFinite,
-                  fit: BoxFit.fill,
-                ),
-              ),
-
-              shadeNumber(
-                number: 4,
-                row: 1,
-                column: 1,
-                height: height * 0.01,
-                width: width * 0.01,
-                percentX: percentX,
-                percentY: percentY,
-                ratio: ratio,
-              ),
-              // shadeNumber(
-              //   number: 4,
-              //   row: 2,
-              //   column: 2,
-              //   height: height * 0.01,
-              //   width: width * 0.01,
-              //   percentX: percentX,
-              //   percentY: percentY,
-              //   ratio: ratio,
-              // ),
-
-              // Score :
-              Positioned(
-                top: height * 0.05,
-                left: width * 0.05,
-                child: Text(
-                  '803.00',
-                  style: style,
-                ),
-              ),
-
-              // Counter:
-              Positioned(
-                top: height * 0.05,
-                left: width * 0.28,
-                child: Counter(
-                  ratio: ratio,
-                  startMinutes: 5,
-                  startSeconds: 0,
-                  style: style,
-                ),
-              ),
-
-              // Winner
-              Positioned(
-                top: height * 0.05,
-                left: width * 0.5,
-                child: Text(
-                  '9',
-                  style: style,
-                ),
-              ),
-
-              // Cancel button
-              Positioned(
-                left: width * 0.6,
-                top: height * 0.133,
-                child: InkWell(
-                  onTap: () {},
+          child: InkWell(
+            onTap: () {
+              print('Position is $percentX and $percentY');
+            },
+            child: Stack(
+              children: [
+                SizedBox(
                   child: Image.asset(
-                    'assets/img/Cancel-Bet.png',
-                    height: height * 0.07,
-                    width: width * 0.1,
+                    'assets/img/SecondMatka.png',
+                    height: double.maxFinite,
+                    width: double.maxFinite,
+                    fit: BoxFit.fill,
                   ),
                 ),
-              ),
 
-              // Cancel All button
-              Positioned(
-                left: width * 0.6,
-                top: height * 0.07,
-                child: InkWell(
-                  onTap: () {},
-                  child: Image.asset(
-                    'assets/img/CancelAll.png',
-                    height: height * 0.07,
-                    width: width * 0.1,
+                // ...List.generate(
+                //   10,
+                //   (index) => shadeNumber(
+                //     row: index + 1,
+                //     column: index + 1,
+                //     number: index + 1,
+                //     percentX: percentX,
+                //     percentY: percentY,
+                //     height: height,
+                //     width: width,
+                //     ratio: ratio,
+                //   ),
+                // ),
+                // Score :
+                Positioned(
+                  top: height * 0.05,
+                  left: width * 0.05,
+                  child: Text(
+                    '803.00',
+                    style: style,
                   ),
                 ),
-              ),
 
-              // Close button
-              Positioned(
-                left: width * 0.65,
-                top: height * 0.02,
-                child: InkWell(
-                  onTap: () {},
-                  child: Image.asset(
-                    'assets/img/Close2.png',
-                    height: height * 0.05,
-                    width: width * 0.1,
+                // Counter:
+                Positioned(
+                  top: height * 0.05,
+                  left: width * 0.28,
+                  child: Counter(
+                    ratio: ratio,
+                    startMinutes: 5,
+                    startSeconds: 0,
+                    style: style,
                   ),
                 ),
-              ),
 
-              // Number Selector:
-
-              Positioned(
-                right: width * 0.04,
-                top: height * 0.03,
-                child: Text(
-                  num < 10 ? '0$num' : num.toString(),
-                  style: style.copyWith(
-                    fontSize: ratio * 40,
+                // Winner
+                Positioned(
+                  top: height * 0.05,
+                  left: width * 0.5,
+                  child: Text(
+                    '9',
+                    style: style,
                   ),
                 ),
-              ),
 
-              // CM/JM Button
-              Positioned(
-                right: width * 0.3,
-                bottom: height * 0.3,
-                child: InkWell(
-                  onTap: () {
-                    setState(() {
-                      showCMJM = !showCMJM;
-                    });
-                  },
-                  child: Image.asset(
-                    'assets/img/CM:JM.png',
-                    height: height * 0.05,
-                    width: width * 0.1,
+                // Cancel button
+                Positioned(
+                  left: width * 0.6,
+                  top: height * 0.133,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Image.asset(
+                      'assets/img/Cancel-Bet.png',
+                      height: height * 0.07,
+                      width: width * 0.1,
+                    ),
                   ),
                 ),
-              ),
 
-              // CM/JM Dialog
-              if (showCMJM)
+                // Cancel All button
+                Positioned(
+                  left: width * 0.6,
+                  top: height * 0.07,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Image.asset(
+                      'assets/img/CancelAll.png',
+                      height: height * 0.07,
+                      width: width * 0.1,
+                    ),
+                  ),
+                ),
+
+                // Close button
+                Positioned(
+                  left: width * 0.65,
+                  top: height * 0.02,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Image.asset(
+                      'assets/img/Close2.png',
+                      height: height * 0.05,
+                      width: width * 0.1,
+                    ),
+                  ),
+                ),
+
+                // Number Selector:
+
+                Positioned(
+                  right: width * 0.04,
+                  top: height * 0.03,
+                  child: Text(
+                    num < 10 ? '0$num' : num.toString(),
+                    style: style.copyWith(
+                      fontSize: ratio * 40,
+                    ),
+                  ),
+                ),
+
+                // CM/JM Button
+                Positioned(
+                  right: width * 0.3,
+                  bottom: height * 0.3,
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        showCMJM = !showCMJM;
+                      });
+                    },
+                    child: Image.asset(
+                      'assets/img/CM:JM.png',
+                      height: height * 0.05,
+                      width: width * 0.1,
+                    ),
+                  ),
+                ),
+
+                // CM/JM Dialog
+                if (showCMJM)
+                  Positioned(
+                    right: 0,
+                    bottom: height * 0.35,
+                    child: Dialog(
+                      backgroundColor: Colors.transparent,
+                      elevation: 10.0,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: DialogWidget(
+                        height: height,
+                        width: width,
+                        ratio: ratio,
+                      ),
+                    ),
+                  ),
+
+                // Take Button
+                Positioned(
+                  right: width * 0.257,
+                  bottom: height * 0.08,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Image.asset(
+                      'assets/img/Take.png',
+                      height: height * 0.0499,
+                      width: width * 0.15,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+
+                // Bet Ok Button
                 Positioned(
                   right: 0,
-                  bottom: height * 0.35,
-                  child: Dialog(
-                    backgroundColor: Colors.transparent,
-                    elevation: 10.0,
-                    shadowColor: Colors.transparent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: DialogWidget(
-                      height: height,
-                      width: width,
-                      ratio: ratio,
+                  bottom: height * 0.08,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Image.asset(
+                      'assets/img/BetOk.png',
+                      height: height * 0.0499,
+                      width: width * 0.15,
+                      fit: BoxFit.fill,
                     ),
                   ),
                 ),
 
-              // Take Button
-              Positioned(
-                right: width * 0.257,
-                bottom: height * 0.08,
-                child: InkWell(
-                  onTap: () {},
-                  child: Image.asset(
-                    'assets/img/Take.png',
-                    height: height * 0.0499,
-                    width: width * 0.15,
-                    fit: BoxFit.fill,
+                // Exit Button
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: InkWell(
+                    onTap: () {
+                      Get.back();
+                    },
+                    child: Image.asset(
+                      'assets/img/Exit 2.png',
+                      height: height * 0.0499,
+                      width: width * 0.085,
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
-              ),
-
-              // Bet Ok Button
-              Positioned(
-                right: 0,
-                bottom: height * 0.08,
-                child: InkWell(
-                  onTap: () {},
-                  child: Image.asset(
-                    'assets/img/BetOk.png',
-                    height: height * 0.0499,
-                    width: width * 0.15,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-
-              // Exit Button
-              Positioned(
-                right: 0,
-                bottom: 0,
-                child: InkWell(
-                  onTap: () {
-                    Get.back();
-                  },
-                  child: Image.asset(
-                    'assets/img/Exit 2.png',
-                    height: height * 0.0499,
-                    width: width * 0.085,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       }),
